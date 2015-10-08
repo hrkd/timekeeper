872a313df618f5f22cfe24ecad77e351ffea598c
hrkd <hiroki@hrkd.net>
2015-10-08 13:29:26 +0900

    install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #see http://qiita.com/Salinger/items/873e3c667462746ae707
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }#
 end
