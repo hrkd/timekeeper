d212a94c00cc4afb58e0adc8a785416b48a436d4
hrkd <hiroki@hrkd.net>
2015-11-02 16:30:16 +0900

    adjust role

--- app/app/models/ability.rb
+++ app/app/models/ability.rb
@@ -29,19 +29,13 @@ class Ability
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
     #
-    can :read, :all                   # allow everyone to read everything
-    if user && user.admin?
-      can :access, :rails_admin       # only allow admin users to access Rails Admin
-      can :dashboard                  # allow access to dashboard
-      if user.role? :superadmin
-        can :manage, :all             # allow superadmins to do anything
-      elsif user.role? :manager
-        can :manage, [User, Product]  # allow managers to do anything to products and users
-      elsif user.role? :sales
-        can :update, Product, :hidden => false  # allow sales to only update visible products
-      end
-    end
 
+    if user && user.admin?
+      can :manage, :all
+    else
+      can :read,  [Task,Work], :hidden => false  # allow sales to only update visible products
+      can :update,[Task,Work], :hidden => false  # allow sales to only update visible products
+    end
 
   end
 end
